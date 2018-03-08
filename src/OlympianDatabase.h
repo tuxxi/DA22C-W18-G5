15d6067af5ff4036da188e75d2c6c2960163ec8e
ajlangley <alexander.langley1@gmail.com>
2018-03-07 21:13:14 -0800

    Overloaded stream insertion operator for Olympian class. Added some comparison functions to OlympianDatabase that will passed to our different data structures for comparisons. Changed file parsing function to use std::strings.

--- src/OlympianDatabase.h
+++ src/OlympianDatabase.h
@@ -7,29 +7,27 @@
 
 class OlympianDatabase {
 private:
-    long (*hash)(const Olympian*, long tableSize);
-    int (*cmpName)(const Olympian*, const Olympian*);
-    int (*cmpAge)(const Olympian*, const Olympian*);
-    int (*cmpHeight)(const Olympian*, const Olympian*);
+    void buildFromFile(std::string);
+    const Olympian *readRecord(ifstream&);
+    bool _insert(Olympian *newRecord);
+    int cmpName(const Olympian*, const Olympian*);
+    int cmpAge(const Olympian*, const Olympian*);
+    int cmpHeight(const Olympian*, const Olympian*);
 
     int nRecords;
-    HashTable<Olympian> *hashTable;
+    HashTable<Olympian, string> *hashTable;
     BinarySearchTree<Olympian> *bstAge;
     BinarySearchTree<Olympian> *bstHeight;
 
-    void buildFromFile(std::string);
-    const Olympian *readRecord(FILE*);
-    bool _insert(const Olympian*);
 public:
     OlympianDatabase(int, int, std::string);
 
     ~OlympianDatabase();
 
     bool remove(std::string);
-    bool insert(const Olympian*);
-    const Olympian* searchByName(std::string);
-    const Olympian* searchByAge(int);
-    const Olympian* searchByHeight(int);
+    const Olympian *searchByName(std::string);
+    const Olympian *searchByAge(int);
+    const Olympian *searchByHeight(int);
 };
 
 #endif //TEAM_PROJECT_OLYMPIANDATABASE_H
